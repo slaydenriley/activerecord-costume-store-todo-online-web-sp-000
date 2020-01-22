cb64ae593f34db510795be344fd7d630cf4741d9
Riley Slayden <slaydenriley@gmail.com>
2020-01-22 16:30:23 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,5 +10,12 @@
 
 class CreateCostumes < ActiveRecord::Migration
   def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
+
+      t.timestamps null: false
   end
 end
